757a78b2712a1bd0d7cdc0db85e366a1adc6678b
Ruslan Kuprieiev <kupruser@gmail.com>
2021-12-02 14:28:57 +0200

    fill out tests

--- dvc_gdrive/tests/fixtures.py
+++ dvc_gdrive/tests/fixtures.py
@@ -1,19 +1,26 @@
-import os
-
 import pytest
+from dvc.fs.gdrive import GDriveFileSystem
 
-
-@pytest.fixture(scope="session")
-def docker_compose_file(pytestconfig):
-    return os.path.join(
-        str(pytestconfig.rootdir), "dvc_gdrive", "tests", "docker-compose.yml"
-    )
+from .cloud import GDrive
 
 
 @pytest.fixture
-def make_gdrive():
+def make_gdrive(make_tmp_dir):
+    if not GDrive.should_test():
+        pytest.skip("no gdrive")
+
     def _make_gdrive():
-        raise NotImplementedError
+        # NOTE: temporary workaround
+        tmp_dir = make_tmp_dir("gdrive", dvc=True)
+
+        ret = GDrive(GDrive.get_url())
+        fs = GDriveFileSystem(
+            gdrive_credentials_tmp_dir=tmp_dir.dvc.tmp_dir, **ret.config
+        )
+        fs.fs._gdrive_create_dir(  # noqa, pylint: disable=protected-access
+            "root", fs.url
+        )
+        yield ret
 
     return _make_gdrive
 
@@ -21,4 +28,3 @@ def make_gdrive():
 @pytest.fixture
 def gdrive(make_gdrive):
     return make_gdrive()
-
