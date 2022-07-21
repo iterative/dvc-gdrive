3c3cec49176affa728bcd9643792a28340d3aeae
Ruslan Kuprieiev <kupruser@gmail.com>
2022-07-21 22:11:59 +0300

    tests: don't create root dir

--- dvc_gdrive/tests/fixtures.py
+++ dvc_gdrive/tests/fixtures.py
@@ -1,5 +1,4 @@
 import pytest
-from dvc.fs import GDriveFileSystem
 
 from .cloud import GDrive
 
@@ -7,20 +6,7 @@ from .cloud import GDrive
 @pytest.fixture
 def make_gdrive():
     def _make_gdrive():
-        from urllib.parse import urlparse
-
-        url = GDrive.get_url()
-        ret = GDrive(url)
-        fs = GDriveFileSystem(**ret.config)
-
-        parsed = urlparse(url)
-        bucket = parsed.hostname
-        path = parsed.path.lstrip("/")
-
-        fs.fs._gdrive_create_dir(  # noqa, pylint: disable=protected-access
-            bucket, path  # noqa, pylint: disable=protected-access
-        )
-        return ret
+        return GDrive(GDrive.get_url())
 
     return _make_gdrive
 
