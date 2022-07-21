b600fcd43a997471b054226264396ebc648bfaa9
Ruslan Kuprieiev <kupruser@gmail.com>
2022-07-21 22:14:39 +0300

    tests: create base dir with cloud

--- dvc_gdrive/tests/fixtures.py
+++ dvc_gdrive/tests/fixtures.py
@@ -6,7 +6,9 @@ from .cloud import GDrive
 @pytest.fixture
 def make_gdrive():
     def _make_gdrive():
-        return GDrive(GDrive.get_url())
+        ret = GDrive(GDrive.get_url())
+        ret.mkdir(ret.url, parents=True, exist_ok=True)
+        return ret
 
     return _make_gdrive
 
