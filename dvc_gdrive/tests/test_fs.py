cdc97b241b827f69a6b8e77a073c77d2538480ac
Ruslan Kuprieiev <kupruser@gmail.com>
2022-07-21 20:57:17 +0300

    tests: test_fs: setenv before creating fs

--- dvc_gdrive/tests/test_fs.py
+++ dvc_gdrive/tests/test_fs.py
@@ -20,23 +20,22 @@ class TestRemoteGDrive:
         assert fs.url == self.CONFIG["url"]
 
     def test_drive(self, dvc, monkeypatch):
-        fs = GDriveFileSystem(
-            gdrive_credentials_tmp_dir=dvc.tmp_dir, **self.CONFIG
-        )
         monkeypatch.setenv(
             GDriveFileSystem.GDRIVE_CREDENTIALS_DATA,
             USER_CREDS_TOKEN_REFRESH_ERROR,
         )
-        with pytest.raises(GDriveAuthError):
-            assert fs.fs
-
-        monkeypatch.setenv(GDriveFileSystem.GDRIVE_CREDENTIALS_DATA, "")
         fs = GDriveFileSystem(
             gdrive_credentials_tmp_dir=dvc.tmp_dir, **self.CONFIG
         )
+        with pytest.raises(GDriveAuthError):
+            assert fs.fs
+
         monkeypatch.setenv(
             GDriveFileSystem.GDRIVE_CREDENTIALS_DATA,
             USER_CREDS_MISSED_KEY_ERROR,
+        )
+        fs = GDriveFileSystem(
+            gdrive_credentials_tmp_dir=dvc.tmp_dir, **self.CONFIG
         )
         with pytest.raises(GDriveAuthError):
             assert fs.fs
