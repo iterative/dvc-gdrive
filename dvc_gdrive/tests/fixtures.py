e7a350c379242fc3559bb5671ea50d1158d9135c
Ruslan Kuprieiev <kupruser@gmail.com>
2022-07-21 19:26:29 +0300

    Update fixtures.py

--- dvc_gdrive/tests/fixtures.py
+++ dvc_gdrive/tests/fixtures.py
@@ -7,15 +7,23 @@ from .cloud import GDrive
 @pytest.fixture
 def make_gdrive(make_tmp_dir):
     def _make_gdrive():
+        from urllib.parse import urlparse
+
         # NOTE: temporary workaround
         tmp_dir = make_tmp_dir("gdrive", dvc=True)
 
-        ret = GDrive(GDrive.get_url())
+        url = GDrive.get_url()
+        ret = GDrive(url)
         fs = GDriveFileSystem(
             gdrive_credentials_tmp_dir=tmp_dir.dvc.tmp_dir, **ret.config
         )
+
+        parsed = urlparse(url)
+        bucket = parsed.hostname
+        path = parsed.path.lstrip("/")
+        
         fs.fs._gdrive_create_dir(  # noqa, pylint: disable=protected-access
-            fs._bucket, fs._path  # noqa, pylint: disable=protected-access
+            bucket, path  # noqa, pylint: disable=protected-access
         )
         return ret
 
