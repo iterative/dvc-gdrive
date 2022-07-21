9c3dcbd6e455713b8f303f8780d06fd10cb259b8
Ruslan Kuprieiev <kupruser@gmail.com>
2022-07-21 22:29:08 +0300

    tests: create temp dir

--- dvc_gdrive/tests/fixtures.py
+++ dvc_gdrive/tests/fixtures.py
@@ -7,6 +7,7 @@ from .cloud import GDrive
 def make_gdrive():
     def _make_gdrive():
         ret = GDrive(GDrive.get_url())
+        ret.mkdir(ret.url)
         return ret
 
     return _make_gdrive
