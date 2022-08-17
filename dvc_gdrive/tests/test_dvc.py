6ff4d980b5d70ffab57d78fea0aaabb9687d6397
Ruslan Kuprieiev <kupruser@gmail.com>
2022-08-18 00:53:54 +0300

    tests: comply with new test framework

--- dvc_gdrive/tests/test_dvc.py
+++ dvc_gdrive/tests/test_dvc.py
@@ -3,52 +3,8 @@ from dvc.testing.test_api import TestAPI  # noqa, pylint: disable=unused-import
 from dvc.testing.test_remote import (  # noqa, pylint: disable=unused-import
     TestRemote,
 )
-from dvc.testing.test_workspace import (  # noqa, pylint: disable=unused-import
-    TestAdd,
-    TestImport,
-)
 
 
 @pytest.fixture
-def cloud_name():
-    return "gdrive"
-
-
-@pytest.fixture
-def remote(make_remote, cloud_name):
-    yield make_remote(name="upstream", typ=cloud_name)
-
-
-@pytest.fixture
-def workspace(make_workspace, cloud_name):
-    pytest.skip("not supported")
-
-
-@pytest.fixture
-def stage_md5():
-    pytest.skip("not supported")
-
-
-@pytest.fixture
-def is_object_storage():
-    pytest.skip("not supported")
-
-
-@pytest.fixture
-def dir_md5():
-    pytest.skip("not supported")
-
-
-@pytest.fixture
-def hash_name():
-    pytest.skip("not supported")
-
-
-@pytest.fixture
-def hash_value():
-    pytest.skip("not supported")
-
-
-@pytest.fixture
-def dir_hash_value(dir_md5):
-    pytest.skip("not supported")
+def remote(make_remote):
+    yield make_remote(name="upstream", typ="gdrive")
