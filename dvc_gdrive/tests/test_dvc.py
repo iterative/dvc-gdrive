9cf86149e2d67b002c71e18e8cb45fc15da2f9e0
Daniele Trifirò <daniele@iterative.ai>
2022-12-07 11:00:05 +0100

    mark broken tests as xfail

--- dvc_gdrive/tests/test_dvc.py
+++ dvc_gdrive/tests/test_dvc.py
@@ -6,13 +6,9 @@ from dvc.testing.remote_tests import (  # noqa, pylint: disable=unused-import
     TestRemote,
 )
 from dvc.testing.workspace_tests import TestAdd as _TestAdd
-from dvc.testing.workspace_tests import (  # noqa, pylint: disable=unused-import
-    TestGetUrl,
-)
+from dvc.testing.workspace_tests import TestGetUrl as _TestGetUrl
 from dvc.testing.workspace_tests import TestImport as _TestImport
-from dvc.testing.workspace_tests import (  # noqa, pylint: disable=unused-import
-    TestLsUrl,
-)
+from dvc.testing.workspace_tests import TestLsUrl as _TestLsUrl
 
 
 @pytest.fixture
@@ -30,9 +26,21 @@ def workspace(make_workspace):
     yield make_workspace(name="workspace", typ="gdrive")
 
 
+@pytest.mark.xfail
 class TestImport(_TestImport):
     pass
 
 
+@pytest.mark.xfail
 class TestAdd(_TestAdd):
+    pass
+
+
+@pytest.mark.xfail
+class TestLsUrl(_TestLsUrl):
+    pass
+
+
+@pytest.mark.xfail
+class TestGetUrl(_TestGetUrl):
     pass
